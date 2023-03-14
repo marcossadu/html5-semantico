102ee9877700d752b08e60788cd7677f09f02365
marcos <masadu@outlook.es>
2023-03-14 14:21:28 +0100

    Terminado proyecto

---
+++ acabado.vb
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+    <!-- Hacer que aparezca en la pestaña del-->
+</head>
+
+<body>
+    <header>
+        <h1>Página principal</h1>
+        <nav>
+            <ul>
+                <li> <a href="http://127.0.0.1:5500/enlaces/enlaces.html"> Enlaces</a>
+                </li>
+                <li><a href="http://127.0.0.1:5500/enlaces/listas.html"> Listas</a></li>
+                <li><a href="http://127.0.0.1:5500/enlaces/formulario.html"> Formularios</a></li>
+                <li><a href="http://127.0.0.1:5500/img/imagenes.html"> Imagenes</a></li>
+                <li>Multimedia<ul>
+                        <li><a href="http://127.0.0.1:5500/enlaces/video.html"> Video</a></li>
+                        <li><a href="http://127.0.0.1:5500/media/audio-files/AUDIO.html"> Audio</a></li>
+                    </ul>
+                <li><a href="http://127.0.0.1:5500/semantica/index.html"> HTML 5 Semantico</a></li>
+                </li>
+                <li><a href="https://practice-html5.vercel.app/enlaces/index.html"> Tablas</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <footer>
+        <p>SINGULARS 2023 </p>
+    </footer>
+</body>
+
+</html>
